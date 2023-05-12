361fbf44d59483e07dbbf099d5ab25a0a0ccac67
eyusias <eyusiasa@gmail.com>
2023-05-11 19:17:57 -0700

    ProjectService is modified for listing and selecting project

--- src/main/java/projects/service/ProjectService.java
+++ src/main/java/projects/service/ProjectService.java
@@ -1,7 +1,9 @@
 package projects.service;
 
+import java.util.List;
 import projects.dao.ProjectDao;
 import projects.entity.Project;
+import projects.exception.DbException;
 
 public class ProjectService {
 
@@ -15,6 +17,25 @@ public class ProjectService {
   public Project addProject(Project project) {
     // Calling insertProject method from project DAO for inserting a project 
     return projectDao.insertProject(project);
+  }
+
+  /**
+   * Calls the project DAO to retrieve all projects.
+   * @return A list of project records
+   */
+  public List<Project> fetchAllProjects() {
+    return projectDao.fetchAllProjects();
+  }
+
+  /**
+   * Calls the project DAO to get a single project details, including materials, steps, and
+   * categories. If the project ID is invalid, it throws an exception.
+   * @param projectId The project ID
+   * @return project object if it is successful
+   */
+  public Project fetchProjectById(Integer projectId) {
+    return projectDao.fetchProjectById(projectId).
+        orElseThrow(() -> new DbException("Project with project ID=" + projectId + " does not exist."));
   }
 
 }
